064309643e4fe30d94f94666ba3dc39cfd797dbf
utkarsha-dev <utkarsha0207@gmail.com>
2020-07-14 16:40:02 +0530

    module 4 solution

---
+++ script.js
@@ -0,0 +1,11 @@
+(function() {
+    var names = ["Umang", "Utkarsha", "Reynold", "Jerry", "Lara", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
+        if (firstLetter === 'j') {
+            byeSpeaker.speak(names[i]);
+        } else {
+            helloSpeaker.speak(names[i]);
+        }
+    }
+})();
